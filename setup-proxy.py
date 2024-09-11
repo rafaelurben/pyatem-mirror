2fae30c20a0013b2d4ffad06bd5932d02a0af6e9
Martijn Braam <martijn@brixit.nl>
2024-09-12 00:07:59 +0200

    Fix up the setup script for the proxy

--- setup-proxy.py
+++ setup-proxy.py
@@ -1,17 +1,21 @@
 # Copyright 2022 - 2022, Martijn Braam and the OpenAtem contributors
 # SPDX-License-Identifier: LGPL-3.0-only
-from setuptools import setup, Extension
+from setuptools import setup
 
 setup(
     name='openswitcher-proxy',
-    version='0.12.0',
+    version='0.12.3',
     packages=['openswitcher_proxy'],
     url='https://git.sr.ht/~martijnbraam/pyatem',
     license='LGPL3',
     author='Martijn Braam',
     author_email='martijn@brixit.nl',
     description='Proxy for ATEM switchers',
-    install_requires=['pyatem'],
+    requires=['pyatem', 'toml'],
+    extras_require={
+        'midi': ['python-rtmidi'],
+        'mqtt': ['paho-mqtt'],
+    },
     classifiers=[
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: Python :: 3',
